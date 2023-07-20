8b3be02a7d29c22c5b91c13bd28c5e7d212ca186
Dylan Knuth <dylan.knuth@oceus.io>
2023-07-19 19:39:36 -0700

    minor edit to web api example

--- examples/cmd/web_api/main.go
+++ examples/cmd/web_api/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"github.com/gravestench/runtime"
-	"github.com/gravestench/runtime/examples/services/cli_flags"
 	"github.com/gravestench/runtime/examples/services/config_file"
 	"github.com/gravestench/runtime/examples/services/web_router"
 	"github.com/gravestench/runtime/examples/services/web_server"
@@ -13,7 +12,6 @@ func main() {
 
 	// will manage the config files for the other services
 	rt.Add(&config_file.Service{})
-	rt.Add(&cli_flags.Service{})
 	rt.Add(&web_server.Service{})
 	rt.Add(&web_router.Service{})
 
