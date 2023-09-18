588db49116fb46aa274ec824a9bd60945af371bc
dylan knuth <dknuth0101@gmail.com>
2023-09-18 15:16:58 -0700

    updating test

--- pkg/runtime_test.go
+++ pkg/runtime_test.go
@@ -3,6 +3,8 @@ package pkg
 import (
 	"testing"
 	"time"
+
+	"github.com/rs/zerolog"
 )
 
 func TestRuntime(t *testing.T) {
@@ -13,5 +15,32 @@ func TestRuntime(t *testing.T) {
 		rt.Shutdown().Wait()
 	}()
 
+	rt.Add(&exampleService{})
+
 	rt.Run()
+}
+
+type exampleService struct {
+	logger *zerolog.Logger
+}
+
+func (e *exampleService) BindLogger(logger *zerolog.Logger) {
+	e.logger = logger
+}
+
+func (e *exampleService) Logger() *zerolog.Logger {
+	return e.logger
+}
+
+func (e *exampleService) Init(rt IsRuntime) {
+
+}
+
+func (e *exampleService) Name() string {
+	return "exmaple"
+}
+
+func (e *exampleService) OnShutdown() {
+	time.Sleep(time.Second * 3)
+	e.logger.Info().Msg("graceful shutdown completed")
 }
